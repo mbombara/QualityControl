dc88229cb4ca0b7501b3014efce08fa85b923421
Marek Bombara <Marek.Bombara@cern.ch>
2021-04-14 09:54:30 +0200

    First steps again...

---
+++ Modules/CTP/include/CTP/Example2Check.h
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   Example2Check.h
+/// \author My Name
+///
+
+#ifndef QC_MODULE_CTP_CTPEXAMPLE2CHECK_H
+#define QC_MODULE_CTP_CTPEXAMPLE2CHECK_H
+
+#include "QualityControl/CheckInterface.h"
+
+namespace o2::quality_control_modules::ctp
+{
+
+/// \brief  Example QC Check
+/// \author My Name
+class Example2Check : public o2::quality_control::checker::CheckInterface
+{
+ public:
+  /// Default constructor
+  Example2Check() = default;
+  /// Destructor
+  ~Example2Check() override = default;
+
+  // Override interface
+  void configure(std::string name) override;
+  Quality check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap) override;
+  void beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult = Quality::Null) override;
+  std::string getAcceptedType() override;
+
+  ClassDefOverride(Example2Check, 1);
+};
+
+} // namespace o2::quality_control_modules::ctp
+
+#endif // QC_MODULE_CTP_CTPEXAMPLE2CHECK_H
